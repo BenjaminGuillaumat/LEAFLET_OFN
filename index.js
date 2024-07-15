9ebea64824f245885521548c121f566254d6855d
BenjaminGuillaumat <120639167+BenjaminGuillaumat@users.noreply.github.com>
2024-07-15 10:02:57 +0200

    filter on company and relationships

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-
 const express = require('express');
 const path = require('path');
-const request = require('request');  // Ajoute ce module pour faire des requêtes HTTP
+const request = require('request'); 
 
 const app = express();
 const PORT = 3000;
